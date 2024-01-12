52f491f166be59225a9e1241a5426983ad0634ba
athulkrishnama <athulkrishnama24@gmail.com>
2024-01-12 14:33:13 +0530

    project initilized

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port 8000.`);
+});
